dda588593008f7db9121ec9d16adddcc5d12c4d2
Kevin Turner <keturn@keturn.net>
2013-09-13 22:21:40 -0700

    whitespace

--- comptroller.js
+++ comptroller.js
@@ -85,7 +85,7 @@ var _Comptroller = function _Comptroller(Game) {
         var scaled, scaledStr, prefixIndex = Math.floor(Math.log(Math.abs(n)) / (Math.LN10 * 3));
         prefixIndex = Math.min(prefixIndex, _prefixes.length - 1);
         scaled = n / (Math.pow(1000, prefixIndex));
-        
+
         if (precision === undefined) {
             precision = 4;
         }
@@ -121,27 +121,30 @@ var _Comptroller = function _Comptroller(Game) {
             beforeThis = document.getElementById("logButton");
             menubar = beforeThis.parentNode;
             menubar.insertBefore(button, beforeThis);
-            
+
             button.onclick = Foundation.toggleComptroller;
             return button;
         },
-        toggleComptroller: function toggleComptroller () {
+        toggleComptroller: function toggleComptroller() {
             // Game.ShowMenu doesn't know what comptroller is, but at least with the current 
             // implementation (1.035) it'll clear the menu area and keep track of the fact that
             // comptroller is using it.
             Game.ShowMenu("comptroller");
         },
-        addComptroller: function addComptroller () {
+        addComptroller: function addComptroller() {
             var rootElement;
             rootElement = Foundation.addToDOM();
             angular.bootstrap(rootElement, ["cookieComptroller"]);
             return rootElement;
         },
-        installPrereqs: function  installPrereqs (callback) {
+        installPrereqs: function installPrereqs(callback) {
             Foundation.loadCSS();
-            loadAngular(function () { defineServices(); callback(); });
+            loadAngular(function () {
+                defineServices();
+                callback();
+            });
         },
-        loadCSS: function loadCSS () {
+        loadCSS: function loadCSS() {
             var style = document.createElement("style");
             style.id = "comptrollerStyle";
             style.setAttribute('type', 'text/css');
@@ -188,8 +191,8 @@ var lowFPS = function (Game) {
     // FIXME: Resetting FPS mid-game may distort various counters, including including research
     // and pledges.
     Game.fps = newFPS;
-    
-    Game.baseResearchTime = Math.round(Game.baseResearchTime * ratio);    
+
+    Game.baseResearchTime = Math.round(Game.baseResearchTime * ratio);
     Game.goldenCookie.delay = Math.round(Game.goldenCookie.delay * ratio);
     Game.frenzy = Math.round(Game.frenzy.delay * ratio);
     Game.clickFrenzy = Math.round(Game.clickFrenzy * ratio);
@@ -197,15 +200,15 @@ var lowFPS = function (Game) {
 };
 
 
-var defineServices = function defineServices () {
+var defineServices = function defineServices() {
     "use strict";
     var module = angular.module("cookieComptroller", []);
-    
+
     /* Make the stock Cookie Clicker game injectable into Angular objects. */
-    module.factory("CookieClicker", function ($rootScope) { 
+    module.factory("CookieClicker", function ($rootScope) {
         var origDraw = Game.Draw;
         // monkeypatch the game's Draw function so that Angular data gets updated.
-        if (Game._ccompOrigDraw) { 
+        if (Game._ccompOrigDraw) {
             console.warn("Game.Draw already hooked?");
         } else {
             /*** MAINLOOP HOOK ***/
@@ -218,9 +221,9 @@ var defineServices = function defineServices () {
         }
         return Game; // this is the global Cookie Clicker "Game" instance. 
     });
-    
+
     /* Filters. */
-    module.filter("metricPrefixed", function () { return Comptroller.metricPrefixed;} );
+    module.filter("metricPrefixed", function () { return Comptroller.metricPrefixed;});
 };
 
 var ComptrollerController = function ComptrollerController($scope, CookieClicker) {
@@ -233,11 +236,11 @@ var ComptrollerController = function ComptrollerController($scope, CookieClicker
     $scope.storeUpgrades = function () { return CookieClicker.UpgradesInStore; };
     $scope.enoughDigits = Comptroller.enoughDigits;
     $scope.timeToRepayUpgrade = Comptroller.timeToRepayUpgrade;
-    
+
     $scope.investmentSize = function () { return CookieClicker.cookiesPs * 60 * 20 * 10; };
-    
-    $scope.comptrollerVisible = function () { return CookieClicker.onMenu === "comptroller"}
-    
+
+    $scope.comptrollerVisible = function () { return CookieClicker.onMenu === "comptroller"; };
+
     $scope.store = {
         incrementalValue: function (obj) {
             return obj.storedCps * CookieClicker.globalCpsMult / CookieClicker.cookiesPs;
@@ -259,11 +262,11 @@ var ComptrollerController = function ComptrollerController($scope, CookieClicker
 
 /* Load a script by adding a <script> tag to the document. */
 var loadScript = function (url, callback) {
-  "use strict";
-  var script = document.createElement("script");
-  script.setAttribute("src", url);
-  script.addEventListener('load', callback, false);
-  document.body.appendChild(script);
+    "use strict";
+    var script = document.createElement("script");
+    script.setAttribute("src", url);
+    script.addEventListener('load', callback, false);
+    document.body.appendChild(script);
 };
 
 var loadAngular = function (callback) {
@@ -292,90 +295,90 @@ function execute(functionOrCode) {
 /* This is not a great way to store and edit CSS and HTML! But Chrome userscripts don't 
  * provide a way to bundle other assets besides the javascript. */
 var ComptrollerAssets = {
-    CSS:   ("#comptroller {\n" +
-      "color: white;" +
-      "/* menu container, even when empty, will transparently hover over our content, so we have to one-up it. */\n" +
-      "z-index:1000001; position:absolute; left:16px; right:0px; top:112px;\n" +
-  "}\n\n" +
-  "#comptroller b {\n" + // screw you, reset stylesheets
-      "font-weight: bolder;" +
-  "}\n\n" + 
-  ".comptrollerStore {\n" +
-      "border-collapse: separate; border-spacing: 1px;" +
-  "}\n\n" + 
-  ".comptrollerStore td, th {\n" +
-  "    padding: 1px 1ex;" +
-  "}\n" +
-  ".comptrollerStore td {\n" +
-      "background-color: #000A24;" +
-  "}\n\n" + 
-  ".comptrollerStore tr:nth-of-type(odd) td {\n" +
-      "background-color: #101A3C;" +
-  "}\n\n" +   
-  ".comptrollerStore th {\n" +
-      "font-weight: bolder;" +
-      "background-color: #240A24;" +
-      "vertical-align: bottom;" +
-  "}\n\n" +
-  "#comptroller .pctInput {\n" +
-      "width: 4em;" +
-  "}\n\n" +
-  /* this is very much mirroring the styles of the game's logButton */
-  "#comptrollerButton {\n" +
-    "top: 0; right: -16px;" +
-    "font-size: 80%;" +
-    "padding: 14px 16px 10px 0px;" +
-    "}\n\n" +
-    "#comptrollerButton:hover{right:-8px;}" +
-  "}\n\n"),
+    CSS: ("#comptroller {\n" +
+        "color: white;" +
+        "/* menu container, even when empty, will transparently hover over our content, so we have to one-up it. */\n" +
+        "z-index:1000001; position:absolute; left:16px; right:0px; top:112px;\n" +
+        "}\n\n" +
+        "#comptroller b {\n" + // screw you, reset stylesheets
+        "font-weight: bolder;" +
+        "}\n\n" +
+        ".comptrollerStore {\n" +
+        "border-collapse: separate; border-spacing: 1px;" +
+        "}\n\n" +
+        ".comptrollerStore td, th {\n" +
+        "    padding: 1px 1ex;" +
+        "}\n" +
+        ".comptrollerStore td {\n" +
+        "background-color: #000A24;" +
+        "}\n\n" +
+        ".comptrollerStore tr:nth-of-type(odd) td {\n" +
+        "background-color: #101A3C;" +
+        "}\n\n" +
+        ".comptrollerStore th {\n" +
+        "font-weight: bolder;" +
+        "background-color: #240A24;" +
+        "vertical-align: bottom;" +
+        "}\n\n" +
+        "#comptroller .pctInput {\n" +
+        "width: 4em;" +
+        "}\n\n" +
+        /* this is very much mirroring the styles of the game's logButton */
+        "#comptrollerButton {\n" +
+        "top: 0; right: -16px;" +
+        "font-size: 80%;" +
+        "padding: 14px 16px 10px 0px;" +
+        "}\n\n" +
+        "#comptrollerButton:hover{right:-8px;}" +
+        "}\n\n"),
     HTML: (
-      "<div ng-controller='ComptrollerController' ng-show='comptrollerVisible()'>\n" +
-      /* frenzy? */
-      "<p ng-show='Game.frenzy'>&#xa1;&#xa1;FRENZY!! " + 
-      "{{ Game.frenzyPower * 100 }}% for {{ (Game.frenzy / Game.fps).toFixed(1) }} seconds.</p>\n" +
-      "<p ng-show='Game.clickFrenzy'>&#x2606;&#x2605;&#x2606; CLICK FRENZY!! &#x2606;&#x2605;&#x2606; " + 
-      "{{ Game.computedMouseCps | metricPrefixed }}cookies per click for {{ (Game.clickFrenzy / Game.fps).toFixed(1) }} seconds.</p>\n" +
-      /* total cookies and rates */
-      "<p>{{ Game.cookies | metricPrefixed:enoughDigits(Game.cookies, Game.cookiesPs):true }}cookies " + 
-      "(investment {{ (Game.cookies > investmentSize()) && '+' || '' }}{{ Game.cookies - investmentSize() | metricPrefixed }}cookies) at<br />\n" + 
-      "{{ Game.cookiesPs | metricPrefixed }}cookies per second, {{ Game.cookiesPs * 60 | metricPrefixed }}cookies per minute, or <br />\n" +
-      "{{ timePerCookie() }}.</p>\n" +
-      /* store */
-      "<table class='comptrollerStore'>\n" +
-      /* headers */
-      "<tr><th>Name</th><th>Price<br />(C)</th><th>Price<br />(min)</th>" + 
-      "<th>Incremental<br />Value %</th><th>Time to Repay<br/>(min)</th></tr>\n" +
-      /* objects */
-      "<tbody>\n" +
-      "    <tr ng-repeat='obj in storeObjects()'>" + 
-      "    <td style='text-align: left'>{{ obj.name }}</th>" + 
-      "    <td style='text-align: right'>{{ obj.price | number:0 }}</th>" + 
-      "    <td style='text-align: right'>{{ cookiesToMinutes(obj.price) | number:1 }}</th>" + 
-      "    <td style='text-align: right'>{{ store.incrementalValue(obj) * 100 | number }}%</th>" + 
-      "    <td style='text-align: right'>{{ store.minutesToRepay(obj) | number:1 }}</th>" + 
-      "</tr>\n" +
-      /* upgrades */
-      "<tr ng-repeat='obj in storeUpgrades()'>" +
-      "    <td style='text-align: left' ng-click='$parent.selectedUpgrade = obj'>{{ obj.name }}</th>" + 
-      "    <td style='text-align: right'>{{ obj.basePrice | number:0 }}</th>" + 
-      "    <td style='text-align: right'>{{ cookiesToMinutes(obj.basePrice) | number:1 }}</th>" + 
-      "    <td style='text-align: right'>{{ store.upgradeValue(obj) * 100 || '?' | number }}%</th>" + 
-      "    <td style='text-align: right'>{{ timeToRepayUpgrade(obj.basePrice, store.upgradeValue(obj)) | number:1 }}</th>" + 
-      "</tr>\n" +
-      "</tbody>\n" +
-      /* calculator */
-      "<tbody><tr>" +
+        "<div ng-controller='ComptrollerController' ng-show='comptrollerVisible()'>\n" +
+            /* frenzy? */
+            "<p ng-show='Game.frenzy'>&#xa1;&#xa1;FRENZY!! " +
+            "{{ Game.frenzyPower * 100 }}% for {{ (Game.frenzy / Game.fps).toFixed(1) }} seconds.</p>\n" +
+            "<p ng-show='Game.clickFrenzy'>&#x2606;&#x2605;&#x2606; CLICK FRENZY!! &#x2606;&#x2605;&#x2606; " +
+            "{{ Game.computedMouseCps | metricPrefixed }}cookies per click for {{ (Game.clickFrenzy / Game.fps).toFixed(1) }} seconds.</p>\n" +
+            /* total cookies and rates */
+            "<p>{{ Game.cookies | metricPrefixed:enoughDigits(Game.cookies, Game.cookiesPs):true }}cookies " +
+            "(investment {{ (Game.cookies > investmentSize()) && '+' || '' }}{{ Game.cookies - investmentSize() | metricPrefixed }}cookies) at<br />\n" +
+            "{{ Game.cookiesPs | metricPrefixed }}cookies per second, {{ Game.cookiesPs * 60 | metricPrefixed }}cookies per minute, or <br />\n" +
+            "{{ timePerCookie() }}.</p>\n" +
+            /* store */
+            "<table class='comptrollerStore'>\n" +
+            /* headers */
+            "<tr><th>Name</th><th>Price<br />(C)</th><th>Price<br />(min)</th>" +
+            "<th>Incremental<br />Value %</th><th>Time to Repay<br/>(min)</th></tr>\n" +
+            /* objects */
+            "<tbody>\n" +
+            "    <tr ng-repeat='obj in storeObjects()'>" +
+            "    <td style='text-align: left'>{{ obj.name }}</th>" +
+            "    <td style='text-align: right'>{{ obj.price | number:0 }}</th>" +
+            "    <td style='text-align: right'>{{ cookiesToMinutes(obj.price) | number:1 }}</th>" +
+            "    <td style='text-align: right'>{{ store.incrementalValue(obj) * 100 | number }}%</th>" +
+            "    <td style='text-align: right'>{{ store.minutesToRepay(obj) | number:1 }}</th>" +
+            "</tr>\n" +
+            /* upgrades */
+            "<tr ng-repeat='obj in storeUpgrades()'>" +
+            "    <td style='text-align: left' ng-click='$parent.selectedUpgrade = obj'>{{ obj.name }}</th>" +
+            "    <td style='text-align: right'>{{ obj.basePrice | number:0 }}</th>" +
+            "    <td style='text-align: right'>{{ cookiesToMinutes(obj.basePrice) | number:1 }}</th>" +
+            "    <td style='text-align: right'>{{ store.upgradeValue(obj) * 100 || '?' | number }}%</th>" +
+            "    <td style='text-align: right'>{{ timeToRepayUpgrade(obj.basePrice, store.upgradeValue(obj)) | number:1 }}</th>" +
+            "</tr>\n" +
+            "</tbody>\n" +
+            /* calculator */
+            "<tbody><tr>" +
 //      "    <td><select ng-model='selectedUpgrade' ng-options='u.name for u in storeUpgrades()'></select></th>" +
-      "    <td>{{ selectedUpgrade.name }}</th>" +
-      "    <td style='text-align: right'>{{ selectedUpgrade.basePrice | number:0 }}</th>" + 
-      "    <td style='text-align: right'>{{ cookiesToMinutes(selectedUpgrade.basePrice) | number:1 }}</th>" + 
-      "    <td style='text-align: right'><input type='number' class='pctInput' ng-model='upgradePercent'>%</th>" + 
-      "    <td style='text-align: right'>{{ timeToRepayUpgrade(selectedUpgrade.basePrice, upgradePercent / 100) | number:1 }}</td></th>" + 
-      "</tr>\n<tr>" +
-      "    <td colspan='5' class='description' ng-bind-html-unsafe='selectedUpgrade.desc'></td>\n" +
-      "</tr></tbody>" +
-      "</table>\n" +
-      "</div>\n")
+        "    <td>{{ selectedUpgrade.name }}</th>" +
+        "    <td style='text-align: right'>{{ selectedUpgrade.basePrice | number:0 }}</th>" +
+        "    <td style='text-align: right'>{{ cookiesToMinutes(selectedUpgrade.basePrice) | number:1 }}</th>" +
+        "    <td style='text-align: right'><input type='number' class='pctInput' ng-model='upgradePercent'>%</th>" +
+        "    <td style='text-align: right'>{{ timeToRepayUpgrade(selectedUpgrade.basePrice, upgradePercent / 100) | number:1 }}</td></th>" +
+        "</tr>\n<tr>" +
+        "    <td colspan='5' class='description' ng-bind-html-unsafe='selectedUpgrade.desc'></td>\n" +
+        "</tr></tbody>" +
+        "</table>\n" +
+        "</div>\n")
 };
 
 var boot = function () {
